5b04f5f1ecffd949ba2b76c3b84f0dc8768676b2
iimaansrk <cimraandrama@gmail.com>
2026-05-01 09:42:21 +0300

    Update generate.py

--- TechVJ/generate.py
+++ TechVJ/generate.py
@@ -35,7 +35,7 @@ async def main(bot: Client, message: Message):
         await message.reply("**Your Are Already Logged In. First /logout Your Old Session. Then Do Login.**")
         return 
     user_id = int(message.from_user.id)
-    await message.reply("**How To Create Api Id And Api Hash.\n\nVideo Link :- https://youtu.be/LDtgwpI-N7M**")
+    await message.reply("**How To Create Api Id And Api Hash.\n\nVideo Link :- https://media.khaanfilms.com**")
     api_id_msg = await bot.ask(user_id, "<b>Send Your API ID.\n\nClick On /skip To Skip This Process\n\nNOTE :- If You Skip This Then Your Account Ban Chance Is High.</b>", filters=filters.text)
     if api_id_msg.text == "/skip":
         api_id = API_ID
