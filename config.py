2409e98b3777c9ed4be84cdd5018d6df8aace640
iimaansrk <cimraandrama@gmail.com>
2026-05-01 09:30:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "khaanbarwaani")
+DB_NAME = os.environ.get("DB_NAME", "cimraandrama_db_user")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
 WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) # time in seconds
